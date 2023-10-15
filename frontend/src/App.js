90dff47a184a7ffc3e66e57ce103a0f843379856
nlmanama <nlminhanh12@gmail.com>
2023-10-14 19:15:24 -0700

    Log in to spotify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import {useEffect, useState} from 'react';
+
+
+
 
 function App() {
+  const CLIENT_ID = "b9516d6124f6463990d5e88b10a523bb"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+}, [])
+
+const logout = () => {
+  setToken("")
+  window.localStorage.removeItem("token")
+}
+
+
   return (
     <div className="App">
       <div classname = "header">
         <h>SpotiVibe</h>
       </div>
+      {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                        to Spotify</a>
+                    : <button onClick={logout}>Logout</button>}
     </div>
   );
 }
